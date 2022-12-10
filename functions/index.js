1b31d603af3ac53240a2b4570e1d1ecd5755cd3e
benleys <ben.leys@student.ehb.be>
2022-12-10 17:19:52 +0100

    imports + init main app added

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,29 @@
+//--------------------IMPORTS--------------------
+//Firebase
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
-// // Create and deploy your first functions
-// // https://firebase.google.com/docs/functions/get-started
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+//Express
+const express = require("express");
+//Cors
+const cors = require("cors");
+
+//--------------------MAIN APP--------------------
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+  extended:true
+}));
+//app.use(express.static('public'));
+app.use(cors({ origin: true }));
+
+//--------------------INIT--------------------
+const db = admin.firestore();
+
+//Exports api to firestore
+exports.app = functions.https.onRequest(app);
