9f6e41d09ad164ab0ae4bf284d0866f36242bc22
benleys <ben.leys@student.ehb.be>
2023-01-03 19:00:49 +0100

    validation added

--- functions/index.js
+++ functions/index.js
@@ -12,6 +12,9 @@ admin.initializeApp({
 const express = require("express");
 //Cors
 const cors = require("cors");
+//Joi validation
+const Joi = require("joi");
+const { validateSignup, validateUpdate } = require ("./validator");
 
 //--------------------MAIN APP--------------------
 const app = express();
@@ -25,21 +28,55 @@ app.use(cors({ origin: true }));
 //--------------------INIT--------------------
 const db = admin.firestore();
 const User = db.collection('Users');
+let users = [];
 
 //--------------------ROUTES--------------------
 //Root
 app.get('/', async(req, res) => {
-    //return res.status(200).send("Everything works!");
+    // return res.status(200).send("Everything works!");
     //res.sendFile(__dirname + '/public/index.html');
-    const snapshot = await User.get();
-    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(list);
+    // const snapshot = await User.get();
+    // const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
+    // res.send(list);
+    User.get().then(querySnapshot => {
+      // console.log(`Found ${querySnapshot.size} documents.`);
+      querySnapshot.forEach(doc => {
+          const userDetails = doc.data();
+          users.push(userDetails);
+      });
+  
+      // The array is filled with users
+      // console.log(users.length);
+      res.json(users);
+      users = [];
+    });
+});
+
+app.get('/:id', async(req, res) => {
+    try {
+      const reqDoc = User.doc(req.params.id);
+      let userDetail = await reqDoc.get();
+      let response = userDetail.data();
+
+      return res.status(200).send({ status: "Succes", data: response })
+    } catch (error) {
+      console.log(error)
+      return res.status(500).send({ status: "Failed", data: err })
+    }
+});
+
+app.get('/searchUser/:firstname', async(req, res) => {
+    const firstname = req.params.firstname;
+    console.log(firstname);
+    const stateQueryRes = await User.where('email', '==', 'Bert').get();
+    console.log(stateQueryRes);
+    res.send(stateQueryRes);
 });
 
 //Create User
 app.post('/createUser', async(req, res) => {
-    const data = req.body;
-    await User.add(data);
+    const data = validateSignup(req.body)
+    await User.add(data.value);
     res.send({ msg: "User added successfully"});
 });
 
@@ -47,8 +84,8 @@ app.post('/createUser', async(req, res) => {
 app.put('/updateUser', async(req, res) => {
     const id = req.body.id;
     delete req.body.id;
-    const data = req.body;
-    await User.doc(id).update(data);
+    const data = validateUpdate(req.body);
+    await User.doc(id).update(data.value);
     res.send({ msg: "User updated successfully"});
 });
 
@@ -60,10 +97,19 @@ app.delete('/deleteUser', async(req, res) => {
 });
 
 //Get All Users
-app.get('/getAllUsers', async(req, res) => {
-    const snapshot = await User.get();
-    const list = snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-    res.send(list);
+app.get('/getAll', async(req, res) => {
+  User.get().then(querySnapshot => {
+    // console.log(`Found ${querySnapshot.size} documents.`);
+    querySnapshot.forEach(doc => {
+        const userDetails = doc.data();
+        users.push(userDetails);
+    });
+
+    // The array is filled with users
+    // console.log(users.length);
+    res.json(users);
+    users = [];
+  });
 });
 //Exports api to firestore
 exports.app = functions.https.onRequest(app);
---
+++ functions/validator.js
@@ -0,0 +1,19 @@
+const Joi = require("joi");
+
+const validator = (schema) => (payload) => schema.validate(payload, { abortEarly: false });
+
+const signupSchema = Joi.object({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().min(3).max(10).required(),
+    confirmPassword: Joi.ref('password'),
+});
+
+const updateSchema = Joi.object({
+    firstname: Joi.string().required(),
+    lastname: Joi.string().required(),
+    email: Joi.string().email().required(),
+});
+exports.validateSignup = validator(signupSchema);
+exports.validateUpdate = validator(updateSchema);
